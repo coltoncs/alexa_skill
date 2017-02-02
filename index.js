607f07e63a2efa38c0d130531a1d8fe01a53f1f7
Colton Sweeney <coltonsweeney@Coltons-MacBook-Pro.local>
2017-02-02 09:18:56 -0500

    added compatibility for stop() and unhandled().

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
-var salutations = function(statement) {
-    this.emit(":tell", "Thanks for entertaining me, it was getting boring just sitting here on this table. ");
-    this.emit(":tell", "Now I'm going to turn it over to these humans. ");
-    this.emit(":tell", "I've educated them on how customers want to engage and they will be the best to help you based on your answers. ");
-    this.emit(":tell", "Have a great day!");  
-};
-
 var strings = {
     "YES1": "Great! Do you have a mobile presence? ",
     "YES2": "Great, people have their phones with them all the time. This gives you the ability to reach customers at a moments notice, which is a great opportunity to engage. Do you have a web presence? " ,
@@ -48,30 +41,30 @@ var handlers = {
         counter++;
         switch (counter){
             case 1:
-                //this.emit(":ask", "Great! Do you have a mobile presence? ");
+                //"Great! Do you have a mobile presence? ");
                 this.emit(":ask", strings.YES1);
                 break;
             case 2:
-                //this.emit(":tell", "Great, people have their phones with them all the time. ");
-                //this.emit(":tell", "This gives you the ability to reach customers at a moments notice, which is a great opportunity to engage. ");
-                //this.emit(":ask", "Do you have a web presence? ");
+                //"Great, people have their phones with them all the time. ");
+                //"This gives you the ability to reach customers at a moments notice, which is a great opportunity to engage. ");
+                //"Do you have a web presence? ");
                 this.emit(":ask", strings.YES2);
                 break;
             case 3:
-                //this.emit(":tell", "Great because websites are so 1998. ");
-                //this.emit(":ask", "Do you have a social presence? ");
+                //"Great because websites are so 1998. ");
+                //"Do you have a social presence? ");
                 this.emit(":ask", strings.YES3);
                 break;
             case 4:
-                //this.emit(":tell", "I knew that you were smart when I heard your voice. ");
-                //this.emit(":tell", "If anyone here hasn't engaged with social because they don't think their customers are using it, let's have a conversation at the genius bar tomorrow. ");
-                //this.emit(":ask", "Anyways, do you have a strategy for chatbots, virtual reality, and voice? ")
+                //"I knew that you were smart when I heard your voice. ");
+                //"If anyone here hasn't engaged with social because they don't think their customers are using it, let's have a conversation at the genius bar tomorrow. ");
+                //"Anyways, do you have a strategy for chatbots, virtual reality, and voice? ")
                 this.emit(":ask", strings.YES4);
                 break;
             case 5:
-                //this.emit(":tell", "You are a future thinker. ");
-                //this.emit(":tell", "But seriously, having a plan for chatbots, virtual reality, and voice is critical to staying ahead of changes to customer engagement. ");
-                //this.emit(":ask", "Do you currently have a multi-channel strategy? ");
+                //"You are a future thinker. ");
+                //"But seriously, having a plan for chatbots, virtual reality, and voice is critical to staying ahead of changes to customer engagement. ");
+                //"Do you currently have a multi-channel strategy? ");
                 this.emit(":ask", strings.YES5);
                 break;
             case 6:
@@ -86,35 +79,34 @@ var handlers = {
         counter++;
         switch (counter){
             case 1:
-                //this.emit(":tell", "I guess you don't want to see something cool. Anyone else? ");
-                //this.emit(":ask", "Do you have a mobile presence? ");
+                //"I guess you don't want to see something cool. Anyone else? ");
+                //"Do you have a mobile presence? ");
                 this.emit(":ask", strings.NO1);
                 break;
             case 2:
-                //this.emit(":tell", "If your business doesn't already have a mobile experience, you are running the risk of falling behind competition. ");
-                //this.emit(":ask", "Do you have a web presence? ");
+                //"If your business doesn't already have a mobile experience, you are running the risk of falling behind competition. ");
+                //"Do you have a web presence? ");
                 this.emit(":ask", strings.NO2);
                 break;
             case 3:
-                //this.emit(":tell", "What?! Okay, you are still in the caveman days, let me talk to another volunteer. ");
-                //this.emit(":ask", "Do you have a social presence? ");
+                //"What?! Okay, you are still in the caveman days, let me talk to another volunteer. ");
+                //"Do you have a social presence? ");
                 this.emit(":ask", strings.NO3);
                 break;
             case 4:
-                //this.emit(":tell", "I get it. You are saying that your customers aren't using social. Well you are wrong. We need to get you a plan on social. ");
-                //this.emit(":ask", "Do you have a strategy for chatbots, virtual reality, and voice? ")
+                //"I get it. You are saying that your customers aren't using social. Well you are wrong. We need to get you a plan on social. ");
+                //"Do you have a strategy for chatbots, virtual reality, and voice? ")
                 this.emit(":ask", strings.NO4);
                 break;
             case 5:
-                //this.emit(":tell", "Remember when smartphones were the next big thing? Well these are the new engagement methods of the future.");
-                //this.emit(":tell", "Advancements in artificial intelligence, coupled with the proliferation of messaging apps, are fueling the development of new customer dynamics. ");
-                //this.emit(":ask", "Do you currently have a multi-channel strategy? ");
+                //"Remember when smartphones were the next big thing? Well these are the new engagement methods of the future.");
+                //"Advancements in artificial intelligence, coupled with the proliferation of messaging apps, are fueling the development of new customer dynamics. ");
+                //"Do you currently have a multi-channel strategy? ");
                 this.emit(":ask", strings.NO5);
                 break;
             case 6:
-                //this.emit(":tell", "Did you say no because you don't know what mulit-channel is? The last person tried to hide that from me. ");
-                //this.emit(":tell", "Multi-channel engagement is an ongoing conversation with a customer in places and points of time relative to that individual. ");
-                //salutations();
+                //"Did you say no because you don't know what mulit-channel is? The last person tried to hide that from me. ");
+                //"Multi-channel engagement is an ongoing conversation with a customer in places and points of time relative to that individual. ");
                 this.emit(":tell", strings.SAL2);
                 break;
             default:
@@ -123,10 +115,12 @@ var handlers = {
         }
     },
     'StopIntent': function () {
-        this.emit(':ask','Are you sure?');
+        this.emit(':tell', "I'm afraid I can't let you do that.");
+        this.shouldEndSession = true;
     },
     Unhandled() {
-        this.emit(':ask', "Sorry, I couldn't understand that.", "What was that?");
+        this.emit(':ask', "Sorry, I couldn't understand that. Let's start over.", "What was that?");
+        this.shouldEndSession = true;
     },
     "SessionEndedRequest": function () {
         console.log("Session ended in help state: " + this.event.request.reason); 
