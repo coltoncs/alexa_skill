ea18cef8bd887d546330744fcb46bb2b2a65ae3d
Colton Sweeney <coltonsweeney@Coltons-MacBook-Pro.local>
2017-02-03 08:38:02 -0500

    add help functionality.

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ exports.handler = function(event, context, callback) {
 
 var strings = {
     "YES1": "Great! Do you have a mobile presence? ",
-    "YES2": "Great, people have their phones with them all the time. This gives you the ability to reach customers at a moments notice, which is a great opportunity to engage. Do you have a web presence? " ,
-    "YES3": "Great because websites are so 1998. Do you have a social presence? ",
-    "YES4": "I knew that you were smart when I heard your voice. If anyone here hasn't engaged with social because they don't think their customers are using it, let's have a conversation at the genius bar tomorrow. Anyways, do you have a strategy for chatbots, virtual reality, and voice? ",
-    "YES5": "You are a future thinker. But seriously, having a plan for chatbots, virtual reality, and voice is critical to staying ahead of changes to customer engagement. Do you currently have a multi-channel strategy? ",
+    "YES2": "Great, people have their phones with them all the time. This gives you the ability to reach customers at a moments notice, which is a great opportunity to engage. Do you have a web presence? " , 
+    "YES3": "Great because websites are so 1998. Do you have a social presence? ", 
+    "YES4": "I knew that you were smart when I heard your voice. If anyone here hasn't engaged with social because they don't think their customers are using it, let's have a conversation at the genius bar tomorrow. Anyways, do you have a strategy for chatbots, virtual reality, and voice? ", 
+    "YES5": "You are a future thinker. But seriously, having a plan for chatbots, virtual reality, and voice is critical to staying ahead of changes to customer engagement. Do you currently have a multi-channel strategy? ", 
     "NO1": "I guess you don't want to see something cool. Anyone else? Do you have a mobile presence? ",
     "NO2": "If your business doesn't already have a mobile experience, you are running the risk of falling behind competition. Do you have a web presence? ",
     "NO3": "What?! Okay, you are still in the caveman days, let me talk to another volunteer. Do you have a social presence? ",
     "NO4": "I get it. You are saying that your customers aren't using social. Well you are wrong. We need to get you a plan on social. Do you have a strategy for chatbots, virtual reality, and voice? ",
     "NO5": "Remember when smartphones were the next big thing? Well these are the new engagement methods of the future. Advancements in artificial intelligence, coupled with the proliferation of messaging apps, are fueling the development of new customer dynamics. Do you currently have a multi-channel strategy? ",
-    "SAL1": "Great, I'm glad that you get the importance of reach of your audience when they want, where they want, and how they want. Thanks for entertaining me, it was getting boring just sitting here on this table. Now I'm going to turn it over to these humans. I've educated them on how customers want to engage and they will be the best to help you based on your answers. Have a great day!",
+    "SAL1": "Great, I'm glad that you get the importance of reach of your audience, when they want, where they want, and how they want. Thanks for entertaining me, it was getting boring just sitting here on this table. Now I'm going to turn it over to these humans, I've educated them on how customers want to engage and they will be the best to help you based on your answers. Have a great day!",
     "SAL2": "Did you say no because you don't know what multi-channel is? The last person tried to hide that from me. Multi-channel engagement is an ongoing conversation with a customer in places and points of time relative to that individual. Thanks for entertaining me, it was getting boring just sitting here on this table. Now I'm going to turn it over to these humans. I've educated them on how customers want to engage and they will be the best to help you based on your answers. Have a great day!"
 }
 
@@ -115,12 +115,14 @@ var handlers = {
         }
     },
     'StopIntent': function () {
-        this.emit(':tell', "I'm afraid I can't let you do that.");
+        this.emit(':tell', "Alright, see you later!");
         this.shouldEndSession = true;
     },
+    'HelpIntent': function() {
+        this.emit(':tell', 'Please respond to my questions with yes, or no. Lets try again.');
+    },
     Unhandled() {
-        this.emit(':ask', "Sorry, I couldn't understand that. Let's start over.", "What was that?");
-        this.shouldEndSession = true;
+        this.emit(':tell', "Sorry, I couldn't understand that.");
     },
     "SessionEndedRequest": function () {
         console.log("Session ended in help state: " + this.event.request.reason); 
